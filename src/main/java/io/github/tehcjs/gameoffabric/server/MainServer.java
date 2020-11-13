9319495f943eba1b2e023dd8221218c7abfe437d
TehcJS <tehcdev@gmail.com>
2020-11-12 21:46:23 -0500

    a

---
+++ src/main/java/io/github/tehcjs/gameoffabric/server/MainServer.java
@@ -0,0 +1,13 @@
+package io.github.tehcjs.gameoffabric.server;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.SERVER)
+public class MainServer implements DedicatedServerModInitializer {
+	@Override
+	public void onInitializeServer() {
+
+	}
+}
